69565c7e32a8f6e16cea8745fa55d0928b378f05
bactoria <bactoria@gmail.com>
2018-11-13 12:49:02 +0900

    Closes #1
    구글 애널리틱스 추가

--- src/main.js
+++ src/main.js
@@ -4,8 +4,25 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import index from './store/index'
+import VueAnalytics from 'vue-analytics'
 
 Vue.config.productionTip = false
+
+let debug
+
+if (process.env.NODE_ENV == 'production') {
+  debug = {
+    enabled: true,
+    trace: false,
+    sendHitTask: true
+  }
+}
+
+Vue.use(VueAnalytics, {
+  id: 'UA-129141159-1',
+  router,
+  debug
+})
 
 /* eslint-disable no-new */
 new Vue({
