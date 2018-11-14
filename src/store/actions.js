42b4fefa5e49fb731529e9c2806921cff69b669d
bactoria <bactoria@gmail.com>
2018-11-15 01:43:46 +0900

    이미지 가져오지 못할 때 오류처리
    "이미지를 불러올 수 없습니다." 알림

--- src/store/actions.js
+++ src/store/actions.js
@@ -8,47 +8,13 @@ var server = (process.env.NODE_ENV === 'development') ?
 export default {
     [Const.GET_IMAGES]: (store, payload) => {
         console.log(server + encodeURIComponent(payload))
-        // fix me
-        // progress bar(or circle)
+        // TODO :: progress bar(or circle)
         axios.get(server + encodeURIComponent(payload))
             .then((response) => {
                 store.commit(Const.GET_IMAGES, response.data)
             })
             .catch((error) => {
-                // fix me
-                // 에러일 때 alert 창이 뜨면 좋을거같은뎅
-                if (error.response) {
-                    const { data } = error.response;
-                    // fix me
-                    // data 분기 처리
-                    // response 예
-                    // "response": {
-                    //     "data": {
-                    //         "status": 403,
-                    //         "statusText": "Forbidden",
-                    //         "data": "..."
-                    //     },
-                    //     "status": 500,
-                    //     "statusText": "",
-                    //     "headers": {
-                    //         "content-type": "application/json"
-                    //     },
-                    //     "config": {
-                    //         "transformRequest": {},
-                    //         "transformResponse": {},
-                    //         "timeout": 0,
-                    //         "xsrfCookieName": "XSRF-TOKEN",
-                    //         "xsrfHeaderName": "X-XSRF-TOKEN",
-                    //         "maxContentLength": -1,
-                    //         "headers": {
-                    //             "Accept": "application/json, text/plain, */*"
-                    //         },
-                    //         "method": "get",
-                    //         "url": "https://yqj6ua44w2.execute-api.ap-northeast-2.amazonaws.com/dev/imgs/https%3A%2F%2Fsearch.naver.com%2Fsearch.naver%3Fie%3DUTF-8%26query%3Da%26sm%3Dchr_hty"
-                    //     },
-                    //     "request": {}
-                    // }
-                }
+              alert("이미지를 불러올 수 없습니다.");
             });
     }
 }
